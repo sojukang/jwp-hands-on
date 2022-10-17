242a22e00fbdf62cbc4fd222f81f5547e375c13f
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-10-17 21:56:45 +0900

    feat: stage2 테스트 통과

--- aop/src/test/java/aop/stage2/AopConfig.java
+++ aop/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,30 @@
 package aop.stage2;
 
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
 
 @Configuration
 public class AopConfig {
 
+    private final PlatformTransactionManager transactionManager;
+
+    public AopConfig(final PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    @Bean
+    public TransactionAdvisor getTransactionAdvisor() {
+        return new TransactionAdvisor(new TransactionAdvice(transactionManager), new TransactionPointcut());
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator getAdvisorAutoProxyCreator() {
+        return new DefaultAdvisorAutoProxyCreator();
+    }
 }
